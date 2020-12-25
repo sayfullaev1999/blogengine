ec059905e3ed2b75fafa08b4c378a587f6ba53d5
sayfullaev1999 <dima.sayfullaev@mail.ru>
2020-12-25 22:32:40 +0500

    Добавление формы для создание Тегов

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.posts_list, name='posts_list_url'),  # Список постов
     path('post/<str:slug>/', views.PostDetail.as_view(), name='post_detail_url'),  # Информация о посте
     path('tags/', views.tags_list, name='tags_list_url'),  # Список тегов
+    path('tag/create/', views.TagCreate.as_view(), name='tag_create_url'),  # Создание тега
     path('tag/<str:slug>/', views.TagDetail.as_view(), name='tag_detail_url'),  # Информация о теге
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+
+from .forms import TagForm
 from .models import Post, Tag
 from .utils import ObjectDetailMixin
 
@@ -22,3 +24,16 @@ def tags_list(request):
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
+
+
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm()
+        return render(request, 'blog/tag_create.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(request, 'blog/tag_create.html', context={'form': bound_form})
